4c3d34aa1410bb90f79d0c0e7976a500e96ca979
otutukingsley <56305995+otutukingsley@users.noreply.github.com>
2021-11-17 15:38:30 -0800

    product screen and basic routing

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useNavigate, NavLink } from 'react-router-dom'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const Header = () => {
@@ -6,16 +7,18 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">e-commerce</Navbar.Brand>
+          <NavLink to="/">
+            <Navbar.Brand>e-commerce</Navbar.Brand>
+          </NavLink>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart">
+              <NavLink to="/cart" style={{ color: 'white'}}>
                 <i className="fas fa-shopping-cart"></i> Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
+              </NavLink>
+              <NavLink to="/signin" style={{ color: 'white', margin: "0 0 0 10px"}}>
                 <i className="fas fa-user"></i> Sign In
-              </Nav.Link>
+              </NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>
