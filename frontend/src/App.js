5d022982c47a57cf7f3df169b6a64a6a92133c6a
otutukingsley <56305995+otutukingsley@users.noreply.github.com>
2021-11-17 13:10:55 -0800

    static homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
-  return <div className="App">HELLO</div>
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen/>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default App
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './App.css'
+import './bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
