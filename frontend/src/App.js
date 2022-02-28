f86962997008bcb1a276ac1da34ea9c2fb2a2ea9
otutukingsley <chineduotutu3@gmail.com>
2022-02-28 08:25:39 -0800

    product lists screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,8 @@ import PlaceOrderScreen from "./screens/PlaceOrderScreen"
 import OrderScreen from "./screens/OrderScreen"
 import AdminUsersListScreen from "./screens/AdminUsersListScreen"
 import UserEditScreen from "./screens/UserEditScreen"
+import ProductListScreen from "./screens/ProductListScreen"
+import ProductEditScreen from "./screens/ProductEditScreen"
 
 const App = () => {
   return (
@@ -26,6 +28,11 @@ const App = () => {
             <Route path="/" element={<HomeScreen />} />
             <Route path="/admin/userlist" element={<AdminUsersListScreen />} />
             <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
+            <Route path="/admin/productlist" element={<ProductListScreen />} />
+            <Route
+              path="/admin/product/:id/edit"
+              element={<ProductEditScreen />}
+            />
             <Route path="/register" element={<RegisterScreen />} />
             <Route path="/login" element={<LoginScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
