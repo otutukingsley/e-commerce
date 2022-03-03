8866d6e0c5a24cb759d20ecde66f9067228dcf0b
otutukingsley <chineduotutu3@gmail.com>
2022-03-03 15:11:30 -0800

    admin endpoint: create and update product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -38,4 +38,55 @@ const deleteSingleProduct = asyncHandler(async (req, res) => {
   }
 })
 
-export { getProducts, getSingleProduct, deleteSingleProduct }
+// @desc Create a product
+//@route POST /api/products
+//@access Private Admin Only
+const createSingleProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  })
+
+  const createdProduct = await product.save()
+  res.status(201).json(createdProduct)
+})
+
+// @desc Edit a product
+//@route PUT /api/products
+//@access Private Admin Only
+const updateSingleProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+    product.name = name
+    product.price = price
+    product.description = description
+    product.image = image
+    product.brand = brand
+    product.category = category
+    product.countInStock = countInStock
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+  } else {
+    res.status(404)
+    throw new Error("Product not found")
+  }
+})
+
+export {
+  getProducts,
+  getSingleProduct,
+  deleteSingleProduct,
+  createSingleProduct,
+  updateSingleProduct,
+}
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -4,13 +4,15 @@ import {
   getProducts,
   getSingleProduct,
   deleteSingleProduct,
+  updateSingleProduct,
+  createSingleProduct,
 } from "../controllers/productController.js"
 import { protect, isAdmin } from "../middleware/authMiddleware.js"
 
 // @desc Fetch all products
 //@route GET /api/products
 //@access Public
-router.route("/").get(getProducts)
+router.route("/").get(getProducts).post(protect, isAdmin, createSingleProduct)
 
 // @desc Fetch a product
 //@route GET /api/products/:id
@@ -19,5 +21,6 @@ router
   .route("/:id")
   .get(getSingleProduct)
   .delete(protect, isAdmin, deleteSingleProduct)
+  .put(protect, isAdmin, updateSingleProduct)
 
 export default router
